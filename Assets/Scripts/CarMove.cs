ffae1abc6fe08c83402d116eb20092276e0e7de4
GR8 <gr8_boi52@yahoo.com>
2020-10-09 16:39:18 +0400

    delete car if off the map

--- Assets/Scripts/CarMove.cs
+++ Assets/Scripts/CarMove.cs
@@ -6,17 +6,27 @@ using UnityEngine.UI;
 public class CarMove : MonoBehaviour
 {
   public float movespeed = 0.2f;
+  public float MaxDist = 500;
   public Vector3 userDirection = Vector3.forward;
+
+  Vector3 spawnLocation;
 
   // Start is called before the first frame update
   void Start()
   {
-
+    spawnLocation = transform.position;
   }
 
   // Update is called once per frame
   void Update()
   {
     transform.Translate(userDirection * movespeed);
+
+    // Delete vehicle when it reached max distance
+    if (Vector3.Distance(spawnLocation, transform.position) > MaxDist)
+    {
+      print("vehicle destroyed");
+      Destroy(gameObject);
+    }
   }
 }
